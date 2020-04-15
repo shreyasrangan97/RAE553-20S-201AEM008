2eabc8d7df98e691a3711f499d61a0a34a48b327
shreyasrangan97 <62551168+shreyasrangan97@users.noreply.github.com>
2020-04-15 23:19:50 +0300

    week 11.2 assignment

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -14,11 +14,18 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self, name):
-            item = {'name': name, 'price': 12.00}
+            data = request.get_json()
+            item = {'name': name, 'price': data['price']}
             items.append(item)
             return item, 201
 
-api.add_resource(Item, '/item/<string:name>')
 
-app.run(port=5000)
+class ItemList(Resource):
+    def get(self):
+        return{'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
 #RAE553_201AEM008
