11323defc6ac1b89c33a1d3dcabfdf852791de9c
shreyasrangan97 <62551168+shreyasrangan97@users.noreply.github.com>
2020-04-28 02:40:08 +0300

    week12

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'shreyas'
 api = Api(app)
 
 items = []
